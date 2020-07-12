346e9a9b158fcf092ee1359ecf3245506f411f2b
김문기 <heartboy08@nate.com>
2020-07-12 10:11:32 +0900

    방 상세 정보 표시 Layout

---
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_20200711/ViewDetailActivity.kt
@@ -0,0 +1,20 @@
+package kr.co.tjoeun.listviewpractice_20200711
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewDetailActivity : BaseActvity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_detail)
+    }
+
+    override fun setupExents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+}
